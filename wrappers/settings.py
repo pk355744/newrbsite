4fd8ae77833c966b950a2fd9747e0cc227075a2e
prasun <pk.amitech@gmail.com>
2022-03-23 21:43:29 +0600

    Update Django for whitenoise static

--- wrappers/settings.py
+++ wrappers/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$a^w$eh7g*&@dt%hfv7qu*a87u(b3$(5ng(f@bj@%9a*2_z+te'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['rbfashion.herokuapp.com']
 
