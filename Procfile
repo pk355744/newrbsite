ca457345ab4f4fc891491ea43da182fd2ae5e6e9
prasun <pk.amitech@gmail.com>
2022-03-26 13:23:23 +0600

    last

--- Procfile
+++ Procfile
@@ -1,3 +1,5 @@
 web: gunicorn wrappers.wsgi:application --log-file - --log-level debug
 python manage.py collectstatic --noinput
 manage.py migrate
+web: python manage.py runserver 0.0.0.0:$PORT
+release: python manage.py migrate
--- wrappers/settings.py
+++ wrappers/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT=os.path.join(BASE_DIR,"/static/")
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_cdn")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_URL = 'login'
